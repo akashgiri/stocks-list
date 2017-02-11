653d6ce3051f34b3742bc243012bd4d80ff191c4
Akash Giri <akashgiri@outlook.com>
2017-02-11 23:56:06 +0530

    Added asset allocation and stock count data

--- getStocksList.py
+++ getStocksList.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-import requests
 import json
 import re
+import requests
 try:
     from BeautifulSoup import BeautifulSoup
 except ImportError:
@@ -26,31 +26,71 @@ except Exception, e:
 detailedPortfolio = {}
 keys = []
 
+## Function to add some additional data to output
+## currently adding asset allocation, i.e. cash, stock and number of stocks held
+def add_additional_data(soup, count, key, detailedPortfolio):
+    stock_allocation = 0
+    cash_allocation = 0
+
+    ## get the asset allocation fields
+    parent = soup.body.find_all(class_=re.compile("pf_assetalloc"))
+    td_list = parent[0].find_all('td')
+    for td in td_list:
+        if td.string == "Stock":
+            stock_allocation = td.next_sibling.next_sibling.string
+        elif td.string == "Cash":
+            cash_allocation = td.next_sibling.next_sibling.string
+
+    ## write the asset allocation and stocks count
+    misc_data = {}
+    misc_data["stock_allocation"] = (misc_data.get("stock_allocation", 0)
+                                     + float(stock_allocation))
+    misc_data["cash_allocation"] = (misc_data.get("cash_allocation", 0)
+                                    + float(cash_allocation))
+    misc_data["number_of_stocks"] = (misc_data.get("number_of_stocks", 0)
+                                    + count)
+
+    detailedPortfolio[key]["miscellaneous"] = misc_data
+
+    return detailedPortfolio
+
 for url in urls:
     print url
     key = url.rpartition('/')[0].rpartition('/')[2]
     keys.append(key)
-    detailedPortfolio[key] = []
-
+    detailedPortfolio.setdefault(key, {})
+    detailedPortfolio[key].setdefault("stocks-data", [])
+    
     ## Fetch detailed portfolio data
-    r = requests.get(url)
+    response = requests.get(url)
 
-    print r.status_code
+    print response.status_code
 
-    if (r.status_code != 200) or (r.content == ""):
+    if (response.status_code != 200) or (response.content == ""):
         print "No content to parse. Please try again!"
         exit()
         
-    page = r.content
+    page = response.content
     soup = BeautifulSoup(page, "lxml")
     text = soup.body.find_all("td", string=re.compile("Equity"))
-        
+    count = 0
+
     for td in text:
-        prev = td.previous_sibling.previous_sibling.a
+        stock_td = td.previous_sibling.previous_sibling.a
+        if not stock_td:
+            stock_td = td.previous_sibling.previous_sibling.span            
+        sector_td = td.next_sibling.next_sibling
+        current = {}
+
         try:
-            detailedPortfolio[key].append(prev.string)
+            current["stock"] = stock_td.string
+            current["sector"] = sector_td.string
+            detailedPortfolio[key]["stocks-data"].append(current)
+            count += 1
         except:
             pass
+
+    add_additional_data(soup, count, key, detailedPortfolio)
 
 outfile = open('stocks-list.json', 'w')
 json.dump(detailedPortfolio, outfile)
