8a9f6d76563adb591209804ebd46a0e641e49aed
Akash Giri <akashgiri@outlook.com>
2017-02-18 23:33:24 +0530

    Added nav change logic

--- nav_change.py
+++ nav_change.py
@@ -4,8 +4,9 @@ import json
 import re
 import requests
 from fuzzywuzzy import fuzz
+from price_changes import get_stock_price_data, nav_change_analysis
 
-DEFAULT_NAV = 100
+GOOGLE_FINANCE_URL = "http://finance.google.com/finance/info?client=ig&q=NSE:"
 
 def get_mf_stock_data():
     stocks_data = open("stocks-list.json", "r")
@@ -26,41 +27,54 @@ def get_matched_stocks_list():
     data_file = open("analysis_data.txt", "w")
     mf_stocks = get_mf_stock_data()
     listed_stocks = get_listed_stocks_dict()
+    all_mf_stocks = []
+
+    weighting_price_change_data = {}
+    matched_stocks_data = {}
 
     for key in mf_stocks.keys():
-        quantum_stocks = mf_stocks[key]["stocks-data"]
-        print "Total stocks in quantum: " + str(len(quantum_stocks))
+        all_mf_stocks = mf_stocks[key]["stocks-data"]
+        print "Total stocks in mf: " + str(len(all_mf_stocks))
         match_count = 0
         matched_stocks = []
         data_file.write("MATCHED STOCKS\n")
-        for stock in quantum_stocks:
-            #print stock["stock"]
+
+        ## loop to generate the matching stocks and the stock codes
+        ## which will be used to fetch the stock data from google finance
+        for stock in all_mf_stocks:
             stock_name = stock["stock"]
             first_letter = stock_name[0].upper()
-            print first_letter
+
+            ## Branch directly to stocks starting letter
+            ## The data for stocks is segregated based on first letter of stock name
             for current_stock in listed_stocks[first_letter]:
-                #print "Present"
-                #print stock_name, listed_stocks[stock_name]
-                ratio = fuzz.partial_ratio(stock_name, current_stock)
+                ## Get the partial ratio from fuzzywuzzy
+                #ratio = fuzz.partial_ratio(stock_name, current_stock)
+                ratio = fuzz.token_sort_ratio(stock_name, current_stock)
                 if ratio > 90:
+                    '''
                     print "FOUND"
                     print stock_name + " :: " + current_stock
                     print "-----------------------------------------------\n"
+                    '''
                     match_count += 1
                     matched_stocks.append(stock_name)
                     data_file.write(stock_name+" ==> "+current_stock+"\n")
-                '''
-                else:
-                print "NOT MATCHED, RATIO : " + str(ratio)
-                print stock_name + " :: " + current_stock
-                print "-----------------------------------------------\n"
-                '''
-                
+
+                    ## Prepare data for matched stocks
+                    ## stock data appended to matched_stocks_data
+                    ## in format: [[STOCK_CODE_1, WEIGHTING_1], [STOCK_CODE_2, WEIGHTING_2], ..]
+                    matched_stocks_data.setdefault(key, [])
+                    stock_code = listed_stocks[first_letter][current_stock]
+                    size = len(matched_stocks_data[key])
+                    matched_stocks_data[key].append([])
+                    matched_stocks_data[key][size].append(stock_code)
+                    matched_stocks_data[key][size].append(stock["weighting"])
 
     print "Total matches : " + str(match_count)
     print "Stocks not matched are: \n"
     data_file.write("\n\nNOT MATCHED STOCKS\n")
-    for s in quantum_stocks:
+    for s in all_mf_stocks:
         stock = s["stock"]
         if stock not in matched_stocks:
             print stock
@@ -69,6 +83,25 @@ def get_matched_stocks_list():
 
     data_file.close()
 
+    matched_stocks_data = append_price_change_data_in_matched_stocks(matched_stocks_data)
+    #print matched_stocks_data
+    nav_change_analysis()
+
+def append_price_change_data_in_matched_stocks(matched_stocks_data):
+    ## Fetch all the price changes for matched stocks with stock codes
+    for key in matched_stocks_data.keys():
+        for data in matched_stocks_data[key]:
+            code = data[0]
+            url = GOOGLE_FINANCE_URL + code
+            change = get_stock_price_data(url, code)
+            data.append(change)
+
+    #print matched_stocks_data
+    with open("change_data.json", "w") as out:
+        json.dump(matched_stocks_data, out)
+    
+    return matched_stocks_data
+        
 def get_stock_matches():
     matched_stocks = get_matched_stocks_list()
 
--- price_changes.py
+++ price_changes.py
@@ -17,22 +17,51 @@ def get_percent_change_in_price(content):
 
     return percent_change
     
-def get_stock_price_data(url):
+def get_stock_price_data(url, code):
     response = requests.get(url)
 
     print response.status_code
     
     if (response.status_code != 200) or (response.content == ""):
-        print "No content received. Exiting!"
-        exit(0)
+        print "No content received. Exiting!", code
+        return 0.0
 
     content = response.content
     
     content_formatted = json.loads(get_parsed_content(content))
     percent_change = get_percent_change_in_price(content_formatted)
-    print content_formatted
-    print "Percent change in price : " + str(percent_change)
-    print type(percent_change)
+    #print content_formatted
+    print "STOCK :: %s,  PRICE PERCENT CHANGE :: %s" % (content_formatted["t"], str(percent_change))
+    
+    return percent_change
 
-url = "http://finance.google.com/finance/info?client=ig&q=NSE:TATAMOTORS"
-get_stock_price_data(url)    
+#url = "http://finance.google.com/finance/info?client=ig&q=NSE:TATAMOTORS"
+#get_stock_price_data(url)
+
+def nav_change_analysis(*args):
+    content = []
+    if args:
+        print "arguments are here"
+        content = args[0]
+        #print content
+    else:
+        data_file = open("change_data.json", "r")
+        content = json.load(data_file)
+        print "Read from file!"
+
+    total = 0
+    total_w = 0
+    for key in content.keys():
+        for current in content[key]:
+            weighting = current[1]
+            weighting = float(weighting)
+            total_w += weighting
+            change = current[2]
+            change = float(change)
+            total += (weighting * change)
+            
+        #print content
+        total_change = str(total/100)
+        print "Expected NAV change for %s :: %s%%" % (key ,total_change)
+
+#nav_change_analysis()
